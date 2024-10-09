0bdd6f5f6f3e98df4dfb9314d93f8d6da62642a1
Karen Segal <ksegal@andrew.cmu.edu>
2024-10-09 14:21:44 -0400

    update config to allow image imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ugc.production.linktr.ee',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
